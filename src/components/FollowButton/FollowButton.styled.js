69348516b5f7422a5a0c0a392beb5ecff7f598f9
Yaroslav Oliinyk <4106677@gmail.com>
2023-04-28 23:22:46 +0300

    Update FollowButton.styled.js

--- src/components/FollowButton/FollowButton.styled.js
+++ src/components/FollowButton/FollowButton.styled.js
@@ -1,5 +1,8 @@
 import styled from 'styled-components';
 
 export default styled.button`
+  &:hover {
+    background: ${props => (props.follow ? `#5CD3A8` : ` #ebd8ff`)};
+  }
   background: ${props => (props.follow ? `#5CD3A8` : ` #ebd8ff`)};
 `;
