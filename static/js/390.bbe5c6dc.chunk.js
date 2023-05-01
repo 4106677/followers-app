fc30e37dd8c22f6b9fc08bd1ab83976457926b05
4106677 <104924953+4106677@users.noreply.github.com>
2023-05-01 06:40:53 +0000

    Deploying to gh-pages from @ 4106677/followers-app@6a4301b54cb00c7ebb87d5780a37a36984d3d924 🚀

---
+++ static/js/390.bbe5c6dc.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfollowers_app=self.webpackChunkfollowers_app||[]).push([[390],{390:function(n,e,r){r.r(e),r.d(e,{default:function(){return a}});var i,s,t=r(791),l=r(689),p=r(168),c=r(444),d=c.ZP.div(i||(i=(0,p.Z)(["\n  padding: 30px;\n"]))),u=(c.ZP.header(s||(s=(0,p.Z)(["\n  display: flex;\n  flex-direction: row;\n  align-items: center;\n\n  justify-content: center;\n\n  padding: 10px;\n"]))),r(184)),a=function(){return(0,u.jsx)(d,{children:(0,u.jsx)(t.Suspense,{children:(0,u.jsx)(l.j3,{})})})}}}]);
+//# sourceMappingURL=390.bbe5c6dc.chunk.js.map
